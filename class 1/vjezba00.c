baedb1b02e0868583dc2636d457bde6479e5efc4
nk38104 <55879009+nk38104@users.noreply.github.com>
2019-10-16 15:09:43 +0200

    Update vjezba00.c

--- class 1/vjezba00.c
+++ class 1/vjezba00.c
@@ -1,19 +1,17 @@
-#pragma warning(disable : 4996)  // ovim iskljucujemo upozorenja radi strcpy jer je nesigurna funkcija, kompajler preporuca strpcy_s 
+#pragma warning(disable : 4996)  // VS recommends strcpy_s instead of strcpy(unsafe function)
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define SIZE(x) (sizeof(x) / sizeof((x)[0]))
 
-int mystrlen(const char*); // funkcija prima string (char*) i vraæa integer koji predstavlja dužinu stringa
-char* mystrcpy(char*, const char*); // funkcija prima 2 stringa i kopira 2. string u 1. string
-int mystrcmp(const char*, const char*); // funkcija prima dva stringa i usporeðuje ih, vraæa broj<0 ako je prvi<drugog stringa (po													  // leksikografskom poretku), broj>0 ako je drugi<prvog inula ako su stringovi jednaki.
+int mystrlen(const char*);
+char* mystrcpy(char*, const char*);
+int mystrcmp(const char*, const char*);
+char* mystrcat(char*, const char*); 
+char* mystrstr(const char*, const char*); 												
+char* reverse(const char*, char*);
 
-char* mystrcat(char*, const char*); // funkcija prima 2 stringa i dodaje 2.string na kraj 1og
-char* mystrstr(const char*, const char*); // funkcija vraæa pokazivaè (unutar prvog stringa) koji pokazuje na prvo pojavljivanje drugog													 stringa, ako ga ne pronaðe funkcija vraæa NULL
-char* reverse(const char*, char*); // prima 2 stringa i kopira 1.string u 2. string naopako (funkcija ne postoji u string.h)
-
-//Napisati testove za funkcije koji æe ispisivati rezultat „my“ verzije funkcije i originalne verzije funkcije
 int main()
 {
 	char str[] = "abcd1234";
@@ -29,7 +27,8 @@ int main()
 	printf("\nString: %s  strcpy: %s  String: %s  mystrcpy: %s\n", str, dest, string, myDest);
 
 	//mystrcmp
-	printf("\nString: %s  cmpstr: %s  strcmp: %d  mystrcmp(1=2): %d cmpstr: %s  mystrcmp(1>2): %d  mystrcmp(1<2): %d\n", str, dest, strcmp(dest, str), mystrcmp(dest, str), myDest, mystrcmp(myDest, dest), mystrcmp(dest, myDest));
+	printf("\nString: %s  cmpstr: %s  strcmp: %d  mystrcmp(1=2): %d cmpstr: %s  mystrcmp(1>2): %d  mystrcmp(1<2): %d\n", <br>
+	       str, dest, strcmp(dest, str), mystrcmp(dest, str), myDest, mystrcmp(myDest, dest), mystrcmp(dest, myDest));
 
 	printf("strlen: %d  mystrlen: %d\n", mystrlen(dest), mystrlen(myDest));
 
@@ -68,13 +67,13 @@ char* mystrcpy(char *dest, const char *src)
 
 int mystrcmp(const char *str1, const char *str2)
 {
-	for (; *str1 && (*str1 == *str2); str1++, str2++); /* vrtimo petlju dok str ne dode do terminatora i dok su znakovi jednaki, kad dodemo do														razlicitih znakova vracamo razliku njihovih ASCII vrijednosti*/
+	for (; *str1 && (*str1 == *str2); str1++, str2++); 			
 	return *str1 - *str2;
 }
 
 char* mystrcat(char *dest, const char *src)
 {
-	char *ptr = (dest + mystrlen(dest)); // pointer stavimo na kraj stringa(+1) i to je startna pozicija za pocetak kopiranja iz drugog stringa
+	char *ptr = (dest + mystrlen(dest));
 
 	for (; *src != '\0'; *ptr = *src, src++, ptr++, *ptr = '\0');
 
@@ -85,8 +84,8 @@ char* mystrstr(const char *str, const char *substr)
 {
 	char* ptr = substr;
 
-	for (; (*ptr != '\0') && (*str != '\0'); str++) {  // u slucaju da nema podstr u str treba provjeravat jesmo dosli do kraja str
-		ptr = (*ptr == *str) ? ++ptr : substr;		  // povecavamo podstr do terminatora ako su isti znakovi ako nisi vrati podstr na pocetak
+	for (; (*ptr != '\0') && (*str != '\0'); str++) {  
+		ptr = (*ptr == *str) ? ++ptr : substr;		 
 	}
 
 	return (*ptr == '\0') ? str -= mystrlen(substr) : NULL;
