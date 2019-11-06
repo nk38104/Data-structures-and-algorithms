fd10929b9430078fdf27c654f54c76c4752e4e09
nk38104 <55879009+nk38104@users.noreply.github.com>
2019-11-06 18:21:06 +0100

    Update vjezba00.c

--- class 1/vjezba00.c
+++ class 1/vjezba00.c
@@ -1,17 +1,16 @@
-#pragma warning(disable : 4996)  // VS recommends strcpy_s instead of strcpy(unsafe function)
+#pragma warning(disable : 4996) 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define SIZE(x) (sizeof(x) / sizeof((x)[0]))
-
 int mystrlen(const char*);
 char* mystrcpy(char*, const char*);
 int mystrcmp(const char*, const char*);
-char* mystrcat(char*, const char*); 
-char* mystrstr(const char*, const char*); 												
-char* reverse(const char*, char*);
+char* mystrcat(char*, const char*);
+char* mystrstr(const char*, const char*);
+char* reverse(const char*, char*); 
 
+//MY STRING.H FUNCTIONS
 int main()
 {
 	char str[] = "abcd1234";
@@ -27,8 +26,7 @@ int main()
 	printf("\nString: %s  strcpy: %s  String: %s  mystrcpy: %s\n", str, dest, string, myDest);
 
 	//mystrcmp
-	printf("\nString: %s  cmpstr: %s  strcmp: %d  mystrcmp(1=2): %d cmpstr: %s  mystrcmp(1>2): %d  mystrcmp(1<2): %d\n",
-	       str, dest, strcmp(dest, str), mystrcmp(dest, str), myDest, mystrcmp(myDest, dest), mystrcmp(dest, myDest));
+	printf("\nString: %s  cmpstr: %s  strcmp: %d  mystrcmp(1=2): %d cmpstr: %s  mystrcmp(1>2): %d  mystrcmp(1<2): %d\n", str, dest, strcmp(dest, str), mystrcmp(dest, str), myDest, mystrcmp(myDest, dest), mystrcmp(dest, myDest));
 
 	printf("strlen: %d  mystrlen: %d\n", mystrlen(dest), mystrlen(myDest));
 
@@ -38,8 +36,8 @@ int main()
 	printf("strcat: %s  mystrcat: %s\n", dest, myDest);
 
 	//mystrstr()
-	char substr1[] = "efg", substr2[] = "78d";
-	char* p = strstr(dest, substr1), *myp = mystrstr(myDest, substr2);
+	char substr1[] = "efg", substr2[] = "aab";
+	char* p = strstr(dest, substr1), *myp = mystrstr(mystrcpy(myDest, "aaaab"), substr2);
 
 	printf("\nString: %s  Podstring: %s  strstr: %s  myString: %s  myPodstring: %s  mystrstr: %s\n", dest, substr1, p, myDest, substr2, myp);
 
@@ -49,9 +47,11 @@ int main()
 
 int mystrlen(const char *str)
 {
-	int count = 0;
+	int count;
 
-	for(; *str != '\0'; count += 1, str++);
+	for (count = 0; *str != '\0'; str++) {
+		count += 1;
+	}
 
 	return count;
 }
@@ -60,14 +60,17 @@ char* mystrcpy(char *dest, const char *src)
 {
 	char *start = dest;
 
-	for (; *src != '\0'; *dest = *src, dest++, src++, *dest = '\0');
+	for (; *src != '\0'; dest++, src++) {
+		*dest = *src;
+	}
+	*dest = '\0';
 
 	return start;
 }
 
 int mystrcmp(const char *str1, const char *str2)
 {
-	for (; *str1 && (*str1 == *str2); str1++, str2++); 			
+	for (; *str1 && (*str1 == *str2); str1++, str2++);
 	return *str1 - *str2;
 }
 
@@ -75,27 +78,44 @@ char* mystrcat(char *dest, const char *src)
 {
 	char *ptr = (dest + mystrlen(dest));
 
-	for (; *src != '\0'; *ptr = *src, src++, ptr++, *ptr = '\0');
-
+	for (; *src != '\0'; src++, ptr++) {
+		*ptr = *src;
+	}
+	*ptr = '\0';
+	
 	return dest;
 }
 
 char* mystrstr(const char *str, const char *substr)
 {
-	char* ptr = substr;
+	char *ptr = substr, *p;
 
-	for (; (*ptr != '\0') && (*str != '\0'); str++) {  
-		ptr = (*ptr == *str) ? ++ptr : substr;		 
+	for (; *str != '\0'; ) {
+		if (*ptr == *str) {
+			for (p = str; ((*p) == (*ptr)) && (*p != '\0') && (*ptr != '\0'); ptr++, p++);
+
+			if (*ptr == '\0')
+				return str;	
+			else {
+				str = (--p);
+				ptr = substr;
+			}
+		}
+		else{
+			str++;
+		}
 	}
-
-	return (*ptr == '\0') ? str -= mystrlen(substr) : NULL;
+	return NULL;
 }
 
 char* reverse(const char *src, char *dest)
 {
 	char *sptr = (src + (mystrlen(src) - 1)), *dptr = dest;
 
-	for (; *src != '\0'; *dptr = *sptr, src++, dptr++, sptr--, *dptr = '\0');
+	for (; *src != '\0'; src++, dptr++, sptr--) {
+		*dptr = *sptr;
+	}
+	*dptr = '\0';
 
 	return dest;
 }
